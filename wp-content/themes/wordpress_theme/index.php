043559d05f40de0372d568ba7b6a4012e0068cc4
Thanduxolo <thanduxolo@thanduxolo@>
2016-09-22 12:44:33 +0200

    Changed

--- wp-content/themes/wordpress_theme/index.php
+++ wp-content/themes/wordpress_theme/index.php
@@ -35,12 +35,12 @@
 
 </div>
 
-<h1>Teams: query_posts</h1>
-<!-- <?php 
-	//query_posts('post_type=teams&posts_per_page=10'); 
+<h1>Teams: query_posts() Loop</h1>
+<?php 
+	query_posts('post_type=teams&posts_per_page=10'); 
 ?>
 <table class="table">
-  <tr class="row">
+  <tr>
       <th>TITLE</th>
       <th>EXCERPT</th>
       <th>THUMBNAIL</th>
@@ -52,34 +52,69 @@
       <td><?php the_excerpt(); ?></td>
       <td><?php echo get_the_post_thumbnail(); ?></td>
     </tr>
+?>
 <?php endwhile; endif; ?>
-  </table> -->
+ </table>
 
 <br /><br /><hr /></br /><br />
 
-<h1>Teams: get_posts</h1>
+<h1>Teams: get_posts() Loop</h1>
 
-<?php 
+ <?php 
   $team_args = array(
 	'post_type' => 'teams',
 	'numberposts' => 5
   );
   $team_members = get_posts($team_args);
 
-  // print_r($team_members);
   // echo"<pre>";
   // print_r ($team_members);
   // echo"</pre>";
 
-  // if($team_members){
-  // 	echo "THERE ARE TEAM MEMBERS<br>";
-  // 	foreach ($team_members as $value) {
-  // 		setup_postdata($value);?>
-   		<?//php the_content(); ?>
+   if($team_members){
+     echo "THERE ARE TEAM MEMBERS<br>";
+     echo "<table class='table'>
+     <tr>
+       <th>TITLE</th>
+       <th>EXCERPT</th>
+       <th>THUMBNAIL</th>
+     </tr>";
+     foreach ($team_members as $post) : setup_postdata($post);
+     	echo "</tr>";
+   		echo"<td>". get_the_title(). "</td>";
+   		echo"<td>". get_the_excerpt(). "</td>";
+   		echo"<td>". get_the_post_thumbnail(). "</td>";
+   		echo"</tr>";
+  	  endforeach;
+   	
+   	  echo"</table>";
    	}
-  	
-   }
 
 ?>
 
+<br /><br /><hr /></br /><br />
+
+<h1>Teams: Default Loop</h1>
+<table>
+  <tr>
+  	<th>TITLE</th>
+  	<th>EXCERPT</th>
+  	<th>THUMBNAIL</th>
+  </tr>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+			<tr>
+      			<td><?php the_title(); ?></td>
+      			<td><?php the_excerpt(); ?></td>
+      			<td><?php echo get_the_post_thumbnail(); ?></td>
+    		</tr>
+		</div>
+	<?php endwhile; ?>
+	<?php else : ?>
+		<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+			<h1>Not Found</h1>
+		</div>
+
+	<?php endif; ?>
+</table>
 <?php get_footer(); ?>
